b553bcd378dccbf6a017666970859de844b00ea6
Nazanin Hajimirarab <xmirarab89@gmail.com>
2017-02-10 11:52:22 +0200

    Update iterative_parsing.py

--- iterative_parsing.py
+++ iterative_parsing.py
@@ -3,19 +3,18 @@
 """
 This code is part of the Udacity quizes throughout the course.
 
-Your task is to use the iterative parsing to process the map file and
+The task is to use the iterative parsing to process the map file and
 find out not only what tags are there, but also how many, to get the
 feeling on how much of which data you can expect to have in the map.
-Fill out the count_tags function. It should return a dictionary with the 
+I fill out the count_tags function. It returns a dictionary with the 
 tag name as the key and number of times this tag can be encountered in 
 the map as value.
-
-Note that your code will be tested with a different data file than the 'example.osm'
 """
 
 import xml.etree.cElementTree as ET
 import pprint
 
+# Change the path with the location of your OSM file
 OSMFILE = '/Users/nazaninmirarab/Desktop/Data Science/P3/Project/Submission2/san-francisco_california_sample.osm'
 
 def count_tags(filename):
